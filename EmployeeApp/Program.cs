a26c90f4c3c56e84201449c339bde82ffd9a56dc
mylenemelo <mhyemelo@yahoo.com>
2026-05-04 21:32:54 -0400

    Add files via upload

--- EmployeeApp/Program.cs
+++ EmployeeApp/Program.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using EmployeeApp;
+﻿using System; // Allows use of Console
 
+// Interface definition
+interface IQuittable
+{
+    void Quit(); // Method that must be implemented by any class using this interface
+}
+
+// Employee class implementing IQuittable interface
+class Employee : IQuittable
+{
+    // Property for first name
+    public string FirstName { get; set; }
+
+    // Property for last name
+    public string LastName { get; set; }
+
+    // Implementation of Quit method from interface
+    public void Quit()
+    {
+        // Message displayed when employee quits
+        Console.WriteLine($"{FirstName} {LastName} has quit the job.");
+    }
+}
+
+// Main program
 class Program
 {
     static void Main(string[] args)
     {
-        Employee employee = new Employee();
+        Employee emp = new Employee();
+        emp.FirstName = "Mylene";
+        emp.LastName = "Melo";
 
-        employee.FirstName = "Mylene";
-        employee.LastName = "Melo";
-
-        IQuittable quittableEmployee = employee;
-
+        IQuittable quittableEmployee = emp;
         quittableEmployee.Quit();
 
         Console.ReadLine();
